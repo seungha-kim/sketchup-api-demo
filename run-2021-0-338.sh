0e197b46fce3954e48a33a3acd3cf5fef4d0ba53
Dan Kim <dan@acon3d.com>
2022-05-23 22:18:52 +0900

    add missing cmake cache entry

--- run-2021-0-338.sh
+++ run-2021-0-338.sh
@@ -6,7 +6,7 @@ BUILD_DIR=$BASE_DIR/build_2021_0_338
 SKP_FILE=$BASE_DIR/demo.skp
 
 mkdir -p $BUILD_DIR
-cmake -S . -B $BUILD_DIR
+cmake -DUSE_SKETCHUP_2021_0_338=ON -S . -B $BUILD_DIR
 cmake --build $BUILD_DIR
 
 $BUILD_DIR/sketchup-api-demo $SKP_FILE
